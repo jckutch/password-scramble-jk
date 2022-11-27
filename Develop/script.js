9f0529d8a10cab2ca008ab582cd00850abd14d03
John Kutch <jkutch2@yahoo.com>
2022-11-27 13:30:48 -0500

    added criteria and messaging

--- Develop/script.js
+++ Develop/script.js
@@ -6,23 +6,21 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*()_+";
+var passwordVar = "";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
+
+    // Password selection criteria
     var lowerCaseSel = confirm("Shall your password to contain lowercase letters?");
     var upperCaseSel = confirm("Shall your password to contain uppercase letters?");
     var numSel = confirm("Shall your password to contain numbers?");
     var symbolSel = confirm("Shall your password to contain special characters?");
-    var passwordLength = prompt("Select a password length of at least 8 characters and no more than 128 characters.");
 
-    // Password selection criteria
-    var lowerCaseSel = 
-  
     passwordText.value = password;
 }
