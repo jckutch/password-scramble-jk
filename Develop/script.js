a87fd1e5ea0526d28656819e4744626ddcf4a407
John Kutch <jkutch2@yahoo.com>
2022-11-27 16:15:41 -0500

    added more criteria for password

--- Develop/script.js
+++ Develop/script.js
@@ -18,9 +18,22 @@ function writePassword() {
 
     // Password selection criteria
     var lowerCaseSel = confirm("Shall your password to contain lowercase letters?");
+    if (lowerCaseSel) {
+        passwordVar += lowerCase;
+    }
     var upperCaseSel = confirm("Shall your password to contain uppercase letters?");
+    if (upperCaseSel) {
+        passwordVar += upperCase;
+    }
     var numSel = confirm("Shall your password to contain numbers?");
+    if (numSel) {
+        passwordVar += numbers;
+    }
     var symbolSel = confirm("Shall your password to contain special characters?");
+    if (symbolSel) {
+        passwordVar += symbols;
+    }
+    var passwordLength = prompt("Select a password length of at least 8 characters and no more than 128 characters.");
 
     passwordText.value = password;
 }
