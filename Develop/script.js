3b2992a2388eccb7e9a8a07efd1fb07b8f8f5d96
John Kutch <jkutch2@yahoo.com>
2022-11-27 18:30:55 -0500

    added generate code, works, but doesn't remove none selected characters

--- Develop/script.js
+++ Develop/script.js
@@ -13,27 +13,38 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
     var passwordText = document.querySelector("#password");
+    passwordText.value === "";
+    var password = "";
 
     // Password selection criteria
-    var lowerCaseSel = confirm("Shall your password to contain lowercase letters?");
+    var lowerCaseSel = confirm("Do you want password to contain lowercase letters?");
     if (lowerCaseSel) {
-        passwordVar += lowerCase;
+        passwordVar = lowerCase;
     }
-    var upperCaseSel = confirm("Shall your password to contain uppercase letters?");
+    var upperCaseSel = confirm("Do you want password to contain UPPERCASE letters?");
     if (upperCaseSel) {
-        passwordVar += upperCase;
+        passwordVar = upperCase;
     }
-    var numSel = confirm("Shall your password to contain numbers?");
-    if (numSel) {
-        passwordVar += numbers;
+    var numberSel = confirm("Do you want password to contain numbers?");
+    if (numberSel) {
+        passwordVar = numbers;
     }
-    var symbolSel = confirm("Shall your password to contain special characters?");
+    var symbolSel = confirm("Do you want password to contain Symbols?");
     if (symbolSel) {
-        passwordVar += symbols;
+        passwordVar = symbols;
     }
     var passwordLength = prompt("Select a password length of at least 8 characters and no more than 128 characters.");
+
+    // Limits password to 8-128 characters and 
+    if (passwordLength < 8 || passwordLength > 128) { 
+        alert ("Password is not between 8 and 128 characters. Please try again.");
+        var passwordLength = prompt("Select a password length of at least 8 characters and no more than 128 characters.")
+    }
+    // Generate password
+    for (var i = 0; i < passwordLength; i++) {
+        password = passwordVar.charAt(Math.floor(Math.random() * passwordVar.length));
+    }
 
     passwordText.value = password;
 }
